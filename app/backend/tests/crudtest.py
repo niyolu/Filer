898c751fe40a0899303e32cf42cde0937cd6ff2a
niyolu <nijolu@live.de>
2023-07-28 15:23:59 +0200

    yeehaw

---
+++ app/backend/tests/crudtest.py
@@ -0,0 +1,145 @@
+import sys
+sys.path.append("..")
+from sqlalchemy.orm import Session
+
+from app import crud, database, config, models
+from app.models import User, Directory, File, StorageObject
+
+import pytest
+
+print(config.get_settings())
+
+models.Base.metadata.create_all(bind=database.engine)
+
+
+@pytest.fixture
+def db_session():
+    session = next(database.get_db())
+    session = database.local_session()
+    try:
+        yield session
+    finally:
+        session.rollback()
+        session.close()
+
+
+# Helper function to create a new user for testing
+def create_test_user(db, username="testuser", hashed_password="testpassword"):
+    return crud.create_user(db, username=username, hashed_password=hashed_password)
+
+# Helper function to create a new group for testing
+def create_test_group(db, groupname="testgroup"):
+    group = models.Group(name=groupname)
+    db.add(group)
+    db.commit()
+    return group
+
+
+@pytest.fixture(autouse=True)
+def setup_test_data(db_session):
+    # Clean up any existing data and ensure a fresh state for each test
+    # db_session.query(models.StorageObject).delete()
+    
+    for user in crud.get_users(db_session):
+        db_session.delete(user)
+        
+    for group in crud.get_groups(db_session):
+        db_session.delete(group)
+        
+    db_session.commit()
+    
+
+# Unit tests
+def test_create_user(db_session):
+    user = create_test_user(db_session)
+    assert user.id is not None
+    assert user.username == "testuser"
+
+def test_get_user_by_username(db_session):
+    user= create_test_user(db_session)
+    found_user = crud.get_user_by_username(db_session, "testuser")
+    assert found_user is not None
+    assert found_user.id == user.id
+
+def test_create_group(db_session):
+    group = create_test_group(db_session)
+    assert group.id is not None
+    assert group.name == "testgroup"
+
+def test_add_user_to_group(db_session):
+    user = create_test_user(db_session)
+    group = create_test_group(db_session)
+    crud.add_user_to_group(db_session, group_id=group.id, user_id=user.id)
+    
+    user_groups = crud.get_user_groups(db_session, user_id=user.id)
+    assert group in user_groups
+    
+def test_change_active_status_for_user_by_username(db_session):
+    user = create_test_user(db_session)
+    assert user.is_active is True
+    
+    updated_user = crud.change_active_status_for_user_by_username(db_session, "testuser", False)
+    assert updated_user.is_active is False
+    
+def test_create_storage_object(db_session):
+    user = create_test_user(db_session)
+    owned_obj = crud.create_storage_object(db_session, user.id, "/", "file.txt", content=b"Content")
+    assert owned_obj.content == b"Content"
+    assert owned_obj.path == "/file.txt"
+    
+    
+def test_share_storage_object_with_user(db_session):
+    user1 = create_test_user(db_session, "user1")
+    user2 = create_test_user(db_session, "user2")
+    shared_obj = crud.create_storage_object(db_session, user1.id, "/", "shared.txt", content=b"Shared content")
+    assert shared_obj in user1.owned_objects
+    share = crud.share_storage_object_with_user(db_session, shared_obj.id, user1.id, user2.id, "R")
+    assert share is not None
+    assert len(user2.shared_objects) == 1
+    assert user2.shared_objects[0] == shared_obj 
+
+
+def test_group_share_storage_object(db_session):
+    user1 = create_test_user(db_session, "user1")
+    user2 = create_test_user(db_session, "user2")
+    group = create_test_group(db_session)
+    group.members.extend([user1, user2])
+    group_shared_obj = crud.create_storage_object(db_session, user2.id, "/", "group_shared.txt", content=b"Group shared content")
+    share = crud.share_storage_object_with_group(db_session, group_shared_obj.id, group.id, "R")
+    assert share is not None
+    assert group_shared_obj in user1.group_memberships[0].shared_objects
+
+def test_get_all_objs(db_session):
+    user = create_test_user(db_session)
+    other_user = create_test_user(db_session, username="testuser2")
+    owned_obj = crud.create_storage_object(db_session, user.id, "/", "owned.txt", content=b"Owned content")
+    shared_obj = crud.create_storage_object(db_session, other_user.id, "/", "shared.txt", content=b"Shared content")
+    group_shared_obj = crud.create_storage_object(db_session, other_user.id, "/", "group_shared.txt", content=b"Group shared content")
+    group = create_test_group(db_session)
+    group.members.extend([user, other_user])
+    share = crud.share_storage_object_with_user(db_session, shared_obj.id, other_user.id, user.id, "R")
+    group_share = crud.share_storage_object_with_group(db_session, group_shared_obj.id, group.id, "R")
+    owned_objs, shared_objs, group_shared_objs = crud.get_all_objs(db_session, user.id)
+
+    assert share
+    assert group_share
+    assert owned_obj in owned_objs
+    assert shared_obj in shared_objs
+    assert group_shared_obj in group_shared_objs[group.name]
+    
+def test_change_user_quota(db_session):
+    user = create_test_user(db_session)
+    new_quota = 1  # 1 MB
+    updated_user = crud.change_user_quota(db_session, user.id, new_quota)
+    assert updated_user.quota == new_quota
+    
+def test_delete_object_by_path(db_session):
+    user = create_test_user(db_session)
+    file_obj = crud.create_storage_object(db_session, user.id, "/", "file.txt", content=b"File content")
+    deleted_obj = crud.delete_object_by_path(db_session, user.id, "/file.txt")
+    assert deleted_obj.id == file_obj.id
+
+def test_delete_user(db_session):
+    user = create_test_user(db_session)
+    deleted_user = crud.delete_user(db_session, user.id)
+    assert deleted_user.id == user.id
---
+++ app/backend/tests/modeltest.py
@@ -0,0 +1,123 @@
+import sys
+sys.path.append("..")
+
+from app import crud, database, config, models
+from app.models import User, Directory, File, StorageObject
+
+print(config.get_settings())
+
+database.Base.metadata.create_all(bind=database.engine)
+
+
+session = next(database.get_db())
+
+# Test data
+def create_sample_data():
+    # Create users
+    users = session.query(models.User).all()
+    # for user in users:
+    #     print(user)
+    #     session.delete(user)
+    
+    session.commit()
+    input("1")
+    
+    session.query(models.User).delete()
+    session.query(models.Group).delete()
+    session.commit()
+    input("2")
+    try:
+        input("3")
+        user1 = User(username="User1", hashed_password="bye")
+        user2 = User(username="User2", hashed_password="bye")
+        user3 = User(username="User3", hashed_password="bye", root=None)
+
+        # Create directories
+        root1 = Directory(name="Root1", path="/", owner=user1)
+        root2 = Directory(name="Root2", path="/", owner=user2)
+        root3 = Directory(name="Root3", path="/", owner=user2)
+        dir1 = Directory(name="Dir1", path="/Dir1", owner=user1)
+        dir2 = Directory(name="Dir2", path="/Dir2", owner=user2)
+        dir3 = Directory(name="Dir3", path="/Dir3", owner=user3)
+        subdir1 = Directory(name="Subdir1", path="/Dir1/Subdir1", owner=user1)
+        
+        user1.root = root1
+        user2.root = root2
+        user3.root = root3
+
+        # Create files
+        file1 = File(name="File1.txt", path="/Dir1/File1.txt", filetype="text", content=b"Hello, this is File1!", owner=user1)
+        file2 = File(name="File2.txt", path="/Dir1/File2.txt", filetype="text", content=b"Hello, this is File2!", owner=user1)
+        file3 = File(name="File3.txt", path="/Dir2/File3.txt", filetype="text", content=b"Hello, this is File3!", owner=user2)
+
+        # Build the hierarchy
+        root1.children.extend([dir1])
+        root2.children.extend([dir2])
+        dir1.children.append(subdir1)
+        dir1.children.extend([file1, file2])
+        dir2.children.append(file3)
+
+        # Add data to the session
+        session.add_all([user1, user2, user3])
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        print("IGNORING:", e)    
+    print_sample_data(session)
+    
+def print_sample_data(session):
+    users = crud.get_users(session)
+    for user in users:
+        print(user)
+        owned_objects = user.owned_objects
+        for obj in owned_objects:
+            print(obj)
+            if isinstance(obj, File):
+                print("content")
+                print(obj.content.decode())
+        
+
+# CRUD Utility Functions
+def create_storage_object(name, path, type, owner, parent=None):
+    storage_object = StorageObject(name=name, path=path, type=type, owner=owner, parent=parent)
+    session.add(storage_object)
+    session.commit()
+    return storage_object
+
+def create_file(name, path, filetype, content, owner, parent=None):
+    file = File(name=name, path=path, filetype=filetype, content=content, owner=owner, parent=parent)
+    session.add(file)
+    session.commit()
+    return file
+
+def create_directory(name, path, owner, parent=None):
+    directory = Directory(name=name, path=path, owner=owner, parent=parent)
+    session.add(directory)
+    session.commit()
+    return directory
+
+def get_storage_object_by_id(id):
+    return session.query(StorageObject).get(id)
+
+def get_file_by_id(id):
+    return session.query(File).get(id)
+
+def get_directory_by_id(id):
+    return session.query(Directory).get(id)
+
+def update_storage_object(storage_object, **kwargs):
+    for key, value in kwargs.items():
+        setattr(storage_object, key, value)
+    session.commit()
+
+def delete_storage_object(storage_object):
+    session.delete(storage_object)
+    session.commit()
+    
+
+
+def main():
+    create_sample_data()
+    
+if __name__ == "__main__":
+    main()

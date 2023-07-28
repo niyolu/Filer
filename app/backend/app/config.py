32b7a82d344ca9745a92be87987e5d0b098732f0
niyolu <nijolu@live.de>
2023-07-28 15:22:22 +0200

    :o

--- app/backend/app/config.py
+++ app/backend/app/config.py
@@ -6,12 +6,18 @@ class Settings(BaseSettings):
     db_user: str = "root"
     db_pw: str
     db_host: str = "localhost"
+    db_database: str = "db"
     
     auth_secret_key: str
     auth_algorithm: str = "HS256"
     auth_access_token_expire_minutes: int = 30
+    
+    app_admin_pw: str
 
-    model_config = SettingsConfigDict(env_file="app/.env")
+    storage_max_objects_per_dir: int = 10
+    storage_quota_mb: int = 500
+
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 @lru_cache()

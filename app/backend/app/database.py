fbec9bf4484a4cc9cfead785962b0cfc11c674e5
niyolu <nijolu@live.de>
2023-07-28 15:22:43 +0200

    for test mocking hdf

--- app/backend/app/database.py
+++ app/backend/app/database.py
@@ -1,14 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-from . import config
+import config
 
 settings: config.Settings = config.get_settings()
 
-
 port = "3306"
-database = "db"
 
-CONN_STR = f"mysql+mysqlconnector://{settings.db_user}:{settings.db_pw}@{settings.db_host}:{port}/{database}"
+CONN_STR = f"mysql+mysqlconnector://{settings.db_user}:{settings.db_pw}@{settings.db_host}:{port}/{settings.db_database}"
 
 engine = create_engine(CONN_STR)
 local_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)

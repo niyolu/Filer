d9a7ccfa08d110ae6f2aff5a4e4d720b358a978c
niyolu <nijolu@live.de>
2023-07-28 15:23:39 +0200

    ist vermutlich alles schrott

--- app/backend/app/schemas.py
+++ app/backend/app/schemas.py
@@ -1,14 +1,20 @@
+from typing import ForwardRef, Optional
 from pydantic import BaseModel
-
+from enum import Enum
 
 class User(BaseModel):
     username: str
     is_active: bool | None = None
+    
+    quota: int
+    used: int
+    
     class Config:
         from_attributes = True
     
 
 class UserInDB(User):
+    id: int
     hashed_password: str
     
 
@@ -25,13 +31,49 @@ class TokenData(BaseModel):
     username: str | None = None
 
 
-# sql tutorial
+class Permission(str, Enum):
+    R = "R"
+    RW = "RW"
 
 
-class ItemBase(BaseModel):
-    title: str
-    description: str | None = None
+Directory_Ref = ForwardRef("Directory")
 
 
-class ItemCreate(ItemBase):
+class StorageObject(BaseModel):
+    name: str
+    path: str
+    type: str
+    
+    owner: User
+    
+    parent: Optional[Directory_Ref]
+    
+    permission: Permission
+    
+
+class File(StorageObject):
+    filetype: str
+    content: bytes
+
+   
+class Directory(StorageObject):
+    children: list[StorageObject]
+
+
+class StorageObjectCreate(BaseModel):
+    destination: str
+    owner: User
+    name: str
+
+
+class FileCreate(StorageObjectCreate):
+    content: bytes
+
+
+class DirectoryCreate(BaseModel):
     pass
+
+
+class Group(BaseModel):
+    name: str
+    members: list[User]

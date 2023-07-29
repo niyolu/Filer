d8bab5efda8465900cf908e63d214517dc2da010
niyolu <nijolu@live.de>
2023-07-29 23:45:30 +0200

    add dicting orm

--- app/backend/app/database.py
+++ app/backend/app/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import declarative_base, sessionmaker
 import config
 
@@ -12,6 +12,15 @@ engine = create_engine(CONN_STR)
 local_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+class SerializableBase(Base):
+    __abstract__ = True
+    def to_dict(self):
+        return {
+            c.key: getattr(self, c.key)
+            for c in inspect(self).mapper.column_attrs
+        }
+
 
 def get_db():
     db = local_session()

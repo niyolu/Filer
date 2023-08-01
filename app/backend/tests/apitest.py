d757f407ca0b3f6783be8669b5fed8d4b5f0cd55
niyolu <nijolu@live.de>
2023-08-01 12:43:49 +0200

    share

--- app/backend/tests/apitest.py
+++ app/backend/tests/apitest.py
@@ -67,6 +67,67 @@ def test_upload_file():
     response = client.post("/storage/file", files={"file": ("test.txt", file_content)}, data={"path": "/test"}, headers=headers)
     assert response.status_code == 200
     assert "name" in response.json()
+    
+# Test sharing with user
+def test_share_with_user():
+    # Create a user1
+    response = client.post("/users/", json={"username": "user1", "password": "password1"})
+    assert response.status_code == 200
+    user1 = response.json()[0]
+    token1 = response.json()[1]
+
+    # Authenticate as user1 and upload a file
+    headers1 = {"Authorization": f"Bearer {token1}"}
+    response = client.post("/storage/file", headers=headers1, files={"file": ("test.txt", "Hello, User1!")}, data={"path": "/user1-files/"})
+    assert response.status_code == 200
+
+    # Share the file with user2
+    response = client.post("/shares/user", headers=headers1, json={"user_name": "user2", "path": "/user1-files/test.txt", "permission": "R"})
+    assert response.status_code == 200
+
+    # Authenticate as user2 and check if the file is visible
+    response = client.get("/storage/", headers=headers2)
+    assert response.status_code == 200
+    assert "/user1-files/test.txt" in response.json()["owned_objects"]["children"]
+
+# Test sharing with group
+def test_share_with_group():
+    # Create a user1
+    response = client.post("/users/", json={"username": "user1", "password": "password1"})
+    assert response.status_code == 200
+    user1 = response.json()[0]
+    token1 = response.json()[1]
+
+    # Authenticate as user1 and upload a file
+    headers1 = {"Authorization": f"Bearer {token1}"}
+    response = client.post("/storage/file", headers=headers1, files={"file": ("test.txt", "Hello, User1!")}, data={"path": "/user1-files/"})
+    assert response.status_code == 200
+
+    # Create a group
+    response = client.post("/groups/", headers=headers1, json={"group_name": "group1"})
+    assert response.status_code == 200
+
+    # Share the file with the group
+    response = client.post("/shares/group", headers=headers1, json={"group_name": "group1", "path": "/user1-files/test.txt", "permission": "RW"})
+    assert response.status_code == 200
+
+    # Authenticate as user1 again and check if the file is visible in the shared group
+    response = client.get("/groups/me", headers=headers1)
+    assert response.status_code == 200
+    assert "/user1-files/test.txt" in response.json()["shared"][0]["children"]
+
+    # Create another user2
+    response = client.post("/users/", json={"username": "user2", "password": "password2"})
+    assert response.status_code == 200
+    user2 = response.json()[0]
+    token2 = response.json()[1]
+
+    # Authenticate as user2 and check if the shared file is visible
+    headers2 = {"Authorization": f"Bearer {token2}"}
+    response = client.get("/storage/", headers=headers2)
+    assert response.status_code == 200
+    assert "/user1-files/test.txt" in response.json()["owned_objects"]["children"]
+
 
 # Add more test cases for other endpoints as needed.
 # Remember to cover different scenarios, error cases, etc.
